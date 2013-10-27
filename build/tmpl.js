cdcb7586f5c64d7dfcba4d430eca0369b035fffc
mattjmorrison <mattjmorrison@mattjmorrison.com>
2013-10-26 22:17:24 -0500

    Step 11: Remove unneeded files from source control

--- build/tmpl.js
+++
@@ -1,9 +0,0 @@
-Ember.TEMPLATES['application'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  
-
-
-  data.buffer.push("<div class=\"container\">\n  <div class=\"page-header\">\n    <h1><strong id=\"message\">Hello World</strong> <small>Hey Everybody</small></h1>\n  </div>\n\n  <div class=\"alert alert-success\">What's up ICC13</div>\n</div>\n");
-  
-});
